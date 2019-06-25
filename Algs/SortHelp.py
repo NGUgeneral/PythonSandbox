c5ad0beccd0a6a9a393db6bb826abd3781e2a939
egor.osaulenko <osaulenko@skyorca.net>
2019-06-25 13:56:55 +0300

    [Algs] implemented basic sorting helper operations

---
+++ Algs/SortHelp.py
@@ -0,0 +1,17 @@
+def exchange(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
+def is_sorted(arr):
+    if arr == None:
+        return False
+    lim = len(arr)
+    if lim < 2:
+        return True
+    i = 1
+    while i < lim:
+        if arr[i] < arr[i - 1]:
+            return False
+        i+=1
+    return True
