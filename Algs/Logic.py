351b60103a7ad4ce8ea8e4d932fe407a1a6dd74f
egor.osaulenko <osaulenko@skyorca.net>
2019-06-25 18:03:53 +0300

    [Algs] Implemented Fibonacci sequence builder;

---
+++ Algs/Logic.py
@@ -0,0 +1,15 @@
+class Logic:
+    def build_fibonacci_sequence(n):
+        if n < 0:
+            raise ValueError("Sequence count should be positive")
+        if n == 0:
+            return []
+        if n == 1:
+            return [0]
+        res = [0,1]
+        if n == 2:
+            return res
+
+        while len(res)<n:
+            res.append(res[-1] + res[-2])
+        return res
